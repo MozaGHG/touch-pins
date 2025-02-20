aaa62cd404d14104e6688d9895a8abb738656f33
MozaGHG <moritz.stolt@gmail.com>
2025-02-20 11:59:32 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
+    basic.setLedColor(0xff9da5)
+})
+input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
+    basic.setLedColor(0x00ff00)
+})
+input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
+    basic.setLedColor(0xffff00)
+})
+input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
+    basic.setLedColor(0x00ffff)
+})
